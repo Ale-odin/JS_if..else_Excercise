0a235c845b51ac5e085d4bb76391828f9b3980f3
Ale-odin <273398914+Ale-odin@users.noreply.github.com>
2026-05-02 11:49:00 +0200

    Complete reverse string, all tests pass

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let separatedString = string.split("");
+let reversedOrder = separatedString.reverse();
+let reversedString = reversedOrder.join("");
 
+return reversedString
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
